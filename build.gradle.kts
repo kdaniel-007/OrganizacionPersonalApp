42ec1f71d89627319767509153c0bb1196f75ba2
kevin.chavez <kdanielcm007@gmail.com>
2025-05-29 11:19:29 -0600

    Actualizacion de coneccion de login con firebase y pantalla de homeactivity con el xml y boton cerrar session

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.10.0" apply false
+    id("com.android.application") version "8.10.1" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
 
